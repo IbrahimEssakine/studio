1caa604dae231239d91a0ef3834d611547fa9d14
Purga2Ry <purga2ryx@gmail.com>
2025-07-15 19:52:45 +0000

    make the light and dark mode button just clickable not drop down

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -33,7 +33,7 @@ export function Header() {
   const { cart } = useCart();
   const { user, logout } = useUser();
   const { dictionary, setLanguage } = useLanguage();
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   const [isClient, setIsClient] = React.useState(false);
 
   React.useEffect(() => {
@@ -148,26 +148,11 @@ export function Header() {
 
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <nav className="flex items-center">
-             <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon">
-                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                  Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                  Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                  System
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+             <Button variant="ghost" size="icon" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+            </Button>
 
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
